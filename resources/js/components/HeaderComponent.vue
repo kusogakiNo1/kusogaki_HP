b7debc74467fe135652f17b21da25b461b02772f
kusogakiNo1 <kusogakiahomaru@icloud.com>
2023-11-19 04:09:31 +0900

    頑張って構築した個人環境をgithubと連携させるためにcommit&push

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- <div class="header_top">
+        <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-2" >
+                <div class="d-flex p-1"><a href="TOP.html"><img class="logo" src="../image/logo/mizu_fuku_boy.png" alt="logo" title="logo"></a></div>
+            </div>
+            <div class="col-10"></div>
+        </div>
+        </div>
+        </div> -->
+        aaaa
+</template>
+
+<script>
+    export default {
+        mounted() {
+            
+        }
+    }
+</script>
